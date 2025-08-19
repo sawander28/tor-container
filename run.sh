d3c9b53223280aab133b675ddcfad8dffa474b96
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-08-19 02:03:29 +0200

    sockpolicy added

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ podman run \
     --publish 127.0.0.1:9050:9050 \
     --publish 127.0.0.1:9051:9051 \
     --publish 127.0.0.1:9052:9052 \
-    --mount type=volume,src=onion,target=/var/lib/tor \
+    --mount type=volume,src=tor,target=/var/lib/tor \
     -t tor-container
+
